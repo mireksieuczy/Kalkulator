bcbc945bfd5b615ee06bf0e7eb712173f8102846
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-07-02 01:35:27 +0200

    Dodaj pliki projektów.

---
+++ Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// doubleeraction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ResultText.Text = string.Empty;
+            CurrentOperationText.Text = string.Empty;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.Text = string.Empty;
+
+            var button = sender as Button;
+            var currentNumber = button.Name[button.Name.Length - 1];
+            CurrentOperationText.Text += currentNumber;
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+
+            }
+            CurrentOperationText.Text += "+";
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+
+            }
+            CurrentOperationText.Text += "-";
+        }
+
+        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+
+            }
+            CurrentOperationText.Text += "*";
+        }
+
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+
+            }
+            CurrentOperationText.Text += "/";
+        }
+
+        private void ButtonResult_Click(object sender, RoutedEventArgs e)
+        {
+
+            var operation = CurrentOperationText.Text;
+            ResultText.Text = CalculateResult(operation).ToString();
+
+            CurrentOperationText.Text = string.Empty;
+        }
+
+        private bool ContainsOperation(string operation) => operation.Contains('+') || operation.Contains('-') || operation.Contains('*') || operation.Contains('/');
+        private int CalculateResult(string operation)
+        {
+
+            if (operation.Contains('+'))
+            {
+                
+                var elements = operation.Split('+');
+                return int.Parse(elements[0]) + int.Parse(elements[1]);
+                
+            }
+            if (operation.Contains('-'))
+            {
+                var elements = operation.Split('-');
+                return int.Parse(elements[0]) - int.Parse(elements[1]);
+                
+            }
+            if (operation.Contains('*'))
+            {
+                var elements = operation.Split('*');
+                return int.Parse(elements[0]) * int.Parse(elements[1]);
+                
+            }
+            if (operation.Contains('/'))
+            {
+                var elements = operation.Split('/');
+                return int.Parse(elements[0]) / int.Parse(elements[1]);
+                
+            }
+            return default;
+        }
+
+
+    }
+}
